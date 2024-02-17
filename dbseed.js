cf2210cb209bd8952adf23112ab847ed7770b0f4
jiratchaya2002 <ninsuay_j@silpakoen.edu>
2024-02-17 16:16:53 +0700

    Complete #2

--- dbseed.js
+++ dbseed.js
@@ -1,9 +1,46 @@
 import  mysql from 'mysql';
 
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+import {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+  } from "@aws-sdk/client-secrets-manager";
+  
+  const secret_name = "prod/v1.0.0/node-rds";
+  
+  const client = new SecretsManagerClient({
+    region: "us-east-1",
+  });
+  
+  let response;
+  
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+  
+  const secret = response.SecretString;
+  
+  const SecretsManagerResponse = JSON.parse(secret);
+
+  const {DB_USERNAME, DB_PASSWORD} = SecretsManagerResponse
+  // Your code goes here
+
 const con = mysql.createConnection({
     host: "jjxxkr.cpik6822ykjd.us-east-1.rds.amazonaws.com",
-    user: "admin",
-    password: "hdJT95lglond0XU5uk5c"
+    user: DB_USERNAME,
+    password: DB_PASSWORD
 });
 
 // connect to the MySQL database
